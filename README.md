bcfc4ce5e475b002634a5c0717453ac696a882ea
Khalan Le <khalankimle@gmail.com>
2024-08-26 11:47:12 -0400

    final working server

--- README.md
+++ README.md
@@ -10,3 +10,6 @@ Create Express middleware.
 Use Express middleware.
 Use a template engine to render views with Express.
 Interact with a self-made API through HTML forms.
+
+This is my self-made API of cocktails! All created by me. 
+Users can search through the cocktails I have created and look up the ingredients and recipes for them.
--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // set view engine
-app.set('view engine', 'cocktails');
+app.set('view engine', 'ejs');
 
 // data
 const cocktails = require('./data/cocktails.js');
@@ -23,9 +23,22 @@ app.use((req, res, next) => {
 })
 
 // routes
+app.get('/', (req, res) => {
+    res.render('index');
+});
 
+app.post('/search', (req, res) => {
+    const cocktailSearch = req.body.cocktailName.toLowerCase();
+    const cocktail = cocktails.find(c => c.name.toLowerCase() === cocktailSearch);
+
+    if(cocktail) {
+        res.render('cocktail', {cocktail});
+    } else {
+        res.render('index', {error: 'Error! Cocktail not found.'});
+    }
+})
 
 // check if server running and listening for requests
 app.listen(PORT, () => {
     console.log(`Server listening on port: ${PORT}`);
-})
+});
